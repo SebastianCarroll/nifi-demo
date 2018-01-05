b496755ea79171998c65b2e0064345536c9c2a1c
Sebastian Carroll <scarroll@hortonworks.com>
2018-01-05 13:45:56 +0000

    Last commit to save progress

--- setup_commands.sh
+++ setup_commands.sh
@@ -11,4 +11,4 @@ sudo yum -y localinstall $JAVA_RPM
 [ ! -f $NIFI_TAR ] && wget http://mirror.serversupportforum.de/apache/nifi/1.0.0-BETA/$NIFI_TAR
 tar xzvf $NIFI_TAR -C /opt
 cd /opt
-./$NIFI_VER/bin/nifi.sh start
+#./$NIFI_VER/bin/nifi.sh start
